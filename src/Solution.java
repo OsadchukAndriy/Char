9d2f07228872078e47be916c0c1cd28eb69f2095
andrejosadcuk <Rudyst.1995@gmail.com>
2021-12-08 16:54:44 +0200

    null

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,36 @@
 import java.sql.Array;
-import java.util.Arrays;
-import java.util.Calendar;
+import java.util.*;
 
 
 public class Solution {
-    public static String[] strings = {"Hello", "Hello", "World", "Java", "Tasks", "World"};
+    public static String[] strings;
 
     public static void main(String[] args) {
-        String[] drhgjgjhgjhgjh = new String[strings.length];
-        for (int i = 0; i < strings.length; i++) {
-            for (int j = i + 1; j < strings.length; j++) {
-                if (strings[i] == strings[j + 1]) {
-                    System.out.print(strings[i] + ", ");
-                } else {
-                    strings[i] = null;
+        Scanner scanner = new Scanner(System.in);
 
+        strings = new String[6];
+        for (int i = 0; i < 6; i++) {
+            strings[i] = scanner.nextLine();
+        }
+
+        for (int i = 0; i < strings.length; i++) {
+            String currentString = strings[i];
+            for (int j = i + 1; j < strings.length; j++) {
+                if (currentString == null) {
+                    break;
+                }
+                if (currentString.equals(strings[j])) {
+                    strings[j] = null;
+                    strings[i] = null;
                 }
             }
         }
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + ", ");
+        }
     }
 }
+
 
 
 
