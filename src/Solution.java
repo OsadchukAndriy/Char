a871e5d5d954a210a395d92a152ff4d2edf62af2
andrejosadcuk <Rudyst.1995@gmail.com>
2021-12-08 12:59:18 +0200

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,32 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Calendar;
+
+
+public class Solution {
+    public static String[] strings = {"Hello", "Hello", "World", "Java", "Tasks", "World"};
+
+    public static void main(String[] args) {
+        String[] d = new String[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = i + 1; j < strings.length; j++) {
+                if (strings[i] == strings[j + 1]) {
+                    System.out.print(strings[i] + ", ");
+                } else {
+                    strings[i] = null;
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
